6729669b2aa0f897e203354f5fd717cbd33c6671
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-27 15:25:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,9 +5,7 @@ class Backer
     @name = name
     @backed_projects = []
   end
-  
-  def add_backer
-  
+ 
   def back_project(project)
     @backed_projects << project
     project.add_backer(self) unless project.backers.include?(self)
